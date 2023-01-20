fc576e494043ef212f600f86d93d95fc2251fc8e
Jozef Maxted <hello@jozefmaxted.co.uk>
2023-01-20 17:09:51 +0000

    Fix missing homepage decoration on safari

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -6,7 +6,7 @@ import Button from '../components/Button.vue';
 <template>
   <LayoutDefault>
     <div class="absolute inset-0 flex justify-center items-center px-12">
-      <svg class="" viewBox="0 0 329 329" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg class="w-full" viewBox="0 0 329 329" fill="none" xmlns="http://www.w3.org/2000/svg">
         <line x1="294.388" y1="37.3191" x2="36.388" y2="295.319" stroke="#777777" stroke-opacity="0.34" />
         <line x1="36.388" y1="36.6119" x2="294.388" y2="294.612" stroke="#777777" stroke-opacity="0.34" />
         <circle cx="164.5" cy="164.5" r="162.5" stroke="#482914" stroke-width="4" />
