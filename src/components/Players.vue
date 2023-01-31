ab96df5387c92e38e2510a2c41d89011ea108da9
Jozef Maxted <hello@jozefmaxted.co.uk>
2023-01-31 15:49:26 +0000

    Fade out inactive player

--- src/components/Players.vue
+++ src/components/Players.vue
@@ -4,13 +4,13 @@ import Player from './Player.vue';
 import { useGameStore } from '../stores/game';
 
 const store = useGameStore();
-const { player1Name, player2Name } = storeToRefs(store);
+const { player1Name, player2Name, gameCurrentPlayer } = storeToRefs(store);
 </script>
 
 <template>
   <div class="flex justify-between items-center">
-    <Player :name="player1Name" :player="1" orientation="left" />
-    <Player :name="player2Name" :player="2" orientation="right" />
+    <Player :name="player1Name" :player="1" orientation="left" :class="{ 'opacity-30': gameCurrentPlayer !== 1 }" />
+    <Player :name="player2Name" :player="2" orientation="right" :class="{ 'opacity-30': gameCurrentPlayer !== 2 }" />
   </div>
 </template>
 
