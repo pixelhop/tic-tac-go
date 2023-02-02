fca71c6a124b1e49fc17d6ae1e084acf2a7171eb
Jozef Maxted <hello@jozefmaxted.co.uk>
2023-02-02 12:35:33 +0000

    Add plausible

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { VuePlausible } from 'vue-plausible';
 import '@fontsource/orbitron';
 import './style.css';
 import App from './App.vue';
@@ -7,6 +8,12 @@ import router from './router';
 
 const app = createApp(App);
 
+if (import.meta.env.VITE_PLAUSIBLE_DOMAIN) {
+  app.use(VuePlausible, {
+    domain: import.meta.env.VITE_PLAUSIBLE_DOMAIN,
+    apiHost: `https://${import.meta.env.VITE_PLAUSIBLE_DOMAIN}`,
+  });
+}
 app.use(createPinia());
 app.use(router);
 
